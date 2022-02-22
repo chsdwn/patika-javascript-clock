24f786ddb70025456e0142d67fe5db6efbf2935a
chsdwn <chasedown@hotmail.com>
2022-02-22 18:27:08 +0300

    Initial commit

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const name = prompt("Please enter your name")
+
+const myNameSpan = document.getElementById("myName")
+myNameSpan.innerHTML = name || "kullanıcı"
+
+const myClockDiv = document.getElementById("myClock")
+myClockDiv.innerHTML = "00:00:00"
+
+const showTime = () => {
+  console.log("showTime")
+  setInterval(() => {
+    const now = new Date()
+    const hours = now.getHours().toString().padStart(2, "0")
+    const minutes = now.getMinutes().toString().padStart(2, "0")
+    const seconds = now.getSeconds().toString().padStart(2, "0")
+
+    myClockDiv.innerHTML = `${hours}:${minutes}:${seconds}`
+  }, 1000)
+}
